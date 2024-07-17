96e21a3e9e1f532f9d704d48f96502459645679a
Nadia Escobar <130033977+nadiadevcoder@users.noreply.github.com>
2024-07-16 22:10:28 -0300

    Add files via upload

---
+++ src/data/CategoryData.js
@@ -0,0 +1,9 @@
+const CategoryData = [
+    { id: 1, nombre: 'Frontend', color: '#FF5733' },
+    { id: 2, nombre: 'Backend', color: '#3399FF' },
+    { id: 3, nombre: 'Innovación', color: '#33FF57' },
+    { id: 4, nombre: 'Gestión', color: '#FF33E6' },
+  ];
+  
+export default CategoryData;
+  
---
+++ src/data/categoriaLookup.js
@@ -0,0 +1,20 @@
+const categoriaLookup = {
+  'front end': {
+    nombre: 'Front End',
+    colorPrimario: '#6BD1FF'
+  },
+
+
+  'back end': {
+    nombre: 'Back End',
+    colorPrimario: '#00C86F'
+  },
+
+
+  'Innovación y Gestión': {
+    nombre: 'Innovación y Gestión',
+    colorPrimario: '#FFBA05'
+  },
+};
+
+export default categoriaLookup;
